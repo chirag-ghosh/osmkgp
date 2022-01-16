3666ec62045ee794d1323e6bea6b1f2a3d1cd12c
chirag828049 <cghosh828049@gmail.com>
2022-01-16 19:50:18 +0530

    Fixed segfault;

--- main.cpp
+++ main.cpp
@@ -16,18 +16,20 @@
 using namespace std;
 using namespace rapidxml;
 
+// not running. will debug later
 xml_node<> *getRootNode(rapidxml::file<> xmlFile) {
     rapidxml::xml_document<> doc;
     doc.parse<0>(xmlFile.data());
-    xml_node<> *rootNode = doc.first_node("osm");
+    static xml_node<> *rootNode = doc.first_node("osm");
 
     return rootNode;
 }
 
-int getNodeCount(xml_node<> *rootNode, char *nodeName) {
+int getNodeCount(xml_node<> *rootNode, const char *nodeName) {
     int nodeCount = 0;
     for (xml_node<> *myNode = rootNode->first_node(nodeName); myNode; myNode = myNode->next_sibling()) {
-        nodeCount++;
+        if (strcmp(myNode->name(), nodeName) == 0)
+            nodeCount++;
     }
     return nodeCount;
 }
@@ -38,6 +40,7 @@ Node *getNodeList(xml_node<> *rootNode, int count) {
     int tempCount = 0;
 
     for (xml_node<> *myNode = rootNode->first_node("node"); myNode; myNode = myNode->next_sibling()) {
+        if (strcmp(myNode->name(), "node") != 0) continue;
         Node newNode;
         newNode.ID = atoi(myNode->first_attribute("id")->value());
         newNode.visible = myNode->first_attribute("visible")->value();
@@ -50,6 +53,7 @@ Node *getNodeList(xml_node<> *rootNode, int count) {
         newNode.longitude = atof(myNode->first_attribute("lon")->value());
 
         for (xml_node<> *tagNode = myNode->first_node("tag"); tagNode; tagNode = tagNode->next_sibling()) {
+            if (strcmp(tagNode->name(), "tag") != 0) continue;
             if (strcmp(tagNode->first_attribute("k")->value(), "name") == 0) {
                 newNode.name = tagNode->first_attribute("v")->value();
             }
@@ -67,21 +71,22 @@ Way *getWayList(xml_node<> *rootNode, int count) {
     int tempCount = 0;
 
     for (xml_node<> *myNode = rootNode->first_node("way"); myNode; myNode = myNode->next_sibling()) {
+        if (strcmp(myNode->name(), "way") != 0) continue;
         Way newNode;
         newNode.ID = atoi(myNode->first_attribute("id")->value());
+        newNode.visible = (char *)malloc((strlen(myNode->first_attribute("visible")->value()) + 1) * sizeof(char));
         newNode.visible = myNode->first_attribute("visible")->value();
         newNode.version = atoi(myNode->first_attribute("version")->value());
         newNode.changeset = atoi(myNode->first_attribute("changeset")->value());
         newNode.timestamp = myNode->first_attribute("timestamp")->value();
         newNode.user = myNode->first_attribute("user")->value();
         newNode.uid = atoi(myNode->first_attribute("uid")->value());
-
         for (xml_node<> *tagNode = myNode->first_node("nd"); tagNode; tagNode = tagNode->next_sibling()) {
-            newNode.nodeRefList.push_back(atoi(myNode->first_attribute("ref")->value()));
+            if (strcmp(tagNode->name(), "nd") != 0) continue;
+            newNode.nodeRefList.push_back(atoi(tagNode->first_attribute("ref")->value()));
         }
-
         for (xml_node<> *tagNode = myNode->first_node("tag"); tagNode; tagNode = tagNode->next_sibling()) {
-            if (strcmp(tagNode->first_attribute("k")->value(), "name") == 0) {
+            if (strcmp(tagNode->name(), "tag") == 0 && strcmp(tagNode->first_attribute("k")->value(), "name") == 0) {
                 newNode.name = tagNode->first_attribute("v")->value();
             }
         }
@@ -94,19 +99,19 @@ Way *getWayList(xml_node<> *rootNode, int count) {
 
 int main(int argc, char const *argv[]) {
     // get xml root node from file
-    xml_node<> *rootNode = getRootNode("map.osm");
+    rapidxml::xml_document<> doc;
+    rapidxml::file<> xmlFile("map.osm");
+    doc.parse<0>(xmlFile.data());
+    xml_node<> *rootNode = doc.first_node("osm");
 
     int nodeCount = getNodeCount(rootNode, "node");
     int wayCount = getNodeCount(rootNode, "way");
 
+    cout << "Total nodes : " << nodeCount << endl;
+    cout << "Total ways : " << wayCount << endl;
+
     Node *nodeList = (Node *)malloc(nodeCount * sizeof(Node));
     Way *wayList = (Way *)malloc(wayCount * sizeof(Way));
-
-    cout << endl
-         << "Total nodes : " << getNodeCount(rootNode, "node") << endl;
-    cout << endl
-         << "Total ways : " << getNodeCount(rootNode, "way") << endl;
-
     nodeList = getNodeList(rootNode, nodeCount);
     wayList = getWayList(rootNode, wayCount);
     return 0;
