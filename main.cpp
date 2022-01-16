088286e4eddd9784498b455196fbc9e09b507d8f
chirag828049 <cghosh828049@gmail.com>
2022-01-16 11:11:25 +0530

    Added rapidxml and wrote count function;

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+#include "rapidxml.hpp"
+#include "rapidxml_utils.hpp"
+
+using namespace std;
+using namespace rapidxml;
+
+int getNodeCount(xml_node<> *rootNode, char *nodeName) {
+    int nodeCount = 0;
+    for (xml_node<> *myNode = rootNode->first_node(nodeName); myNode; myNode = myNode->next_sibling()) {
+        nodeCount++;
+    }
+    return nodeCount;
+}
+
 int main(int argc, char const *argv[]) {
-    std::cout << "Hello World!";
+    rapidxml::file<> xmlFile("map.osm");
+    rapidxml::xml_document<> doc;
+    doc.parse<0>(xmlFile.data());
+
+    xml_node<> *root_node;
+    root_node = doc.first_node("osm");
+
+    cout << endl
+         << "Total nodes : " << getNodeCount(root_node, "node") << endl;
+    cout << endl
+         << "Total ways : " << getNodeCount(root_node, "way") << endl;
     return 0;
 }
